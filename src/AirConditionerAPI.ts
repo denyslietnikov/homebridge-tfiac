6ba739bfa9c34af221235c62b4e5cf8fe6ad362f
Denis Letnikov <denis.letnikov@gmail.com>
2023-11-21 17:29:42 +0200

    fix

--- src/AirConditionerAPI.ts
+++ src/AirConditionerAPI.ts
@@ -1,15 +1,58 @@
 // AirConditionerAPI.ts
 import { EventEmitter } from 'events';
+import * as dgram from 'dgram';
 
 export class AirConditionerAPI extends EventEmitter {
   private readonly ip: string;
+  private readonly port: number;
 
-  constructor(ip: string) {
+  constructor(ip: string, port: number = 7777) {
     super();
     this.ip = ip;
+    this.port = port;
   }
 
-  // TODO: Add basic methods for controlling the air conditioner
+  private sendCommand(command: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const client = dgram.createSocket('udp4');
+      client.send(command, this.port, this.ip, (error) => {
+        client.close();
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async getOnState(): Promise<boolean> {
+    // Command to get the actual state of the air conditioner
+    const command = '<msg msgid="SyncStatusReq" type="Control" seq="12345"><SyncStatusReq></SyncStatusReq></msg>';
+
+    try {
+      // Send the command to get the actual state
+      await this.sendCommand(command);
+      // Assume the state is always on for now; you should update this with the actual logic
+      return true;
+    } catch (error) {
+      // Log the error and return false (assuming the state is off if there's an error)
+      console.error('Error getting actual state:', error);
+      return false;
+    }
+  }
+
+  async turnOn(): Promise<void> {
+    // Command to turn on the air conditioner
+    const command = '<msg msgid="SetMessage" type="Control" seq="1"><SetMessage><TurnOn>on</TurnOn></SetMessage></msg>';
+    await this.sendCommand(command);
+  }
+
+  async turnOff(): Promise<void> {
+    // Command to turn off the air conditioner
+    const command = '<msg msgid="SetMessage" type="Control" seq="12345"><SetMessage><TurnOn>off</TurnOn></SetMessage></msg>';
+    await this.sendCommand(command);
+  }
 }
 
 export default AirConditionerAPI;
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -9,49 +9,52 @@ export class AirConditionerAccessory implements AccessoryPlugin {
   private readonly deviceAPI: AirConditionerAPI;
   private readonly service: Service;
 
-  services: Service[]
-  private swing = 3;
-
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
     this.name = config.name;
     this.api = api;
-    this.services = [];
+
+    // Extracting 'ip' from config
     let ip = '0.0.0.0';
-    if (('ip' in config) && ('mac' in config)) {
+    if ('ip' in config) {
       ip = config['ip'] as string;
     } else {
-      log.error('ip or mac is not provided');
+      log.error('ip is not provided');
     }
-  
+
+    // Creating an instance of AirConditionerAPI
     this.deviceAPI = new AirConditionerAPI(ip);
 
+    // Creating a Switch service
     this.service = new this.api.hap.Service.Switch(this.name);
 
-    // TODO: Add basic characteristics and their handlers
-    // Example:
-    // this.service.getCharacteristic(this.api.hap.Characteristic.On)
-    //   .on('get', this.handleOnGet.bind(this))
-    //   .on('set', this.handleOnSet.bind(this));
+    // Adding characteristic handlers
+    this.service.getCharacteristic(this.api.hap.Characteristic.On)
+      .on('get', this.handleOnGet.bind(this))
+      .on('set', this.handleOnSet.bind(this));
   }
 
-  // TODO: Add characteristic handlers
-  // Example:
-  // private async handleOnGet(callback): Promise<void> {
-  //   this.log.debug('Triggered GET On');
-  //   const isOn = await this.deviceAPI.getOnState();
-  //   callback(null, isOn);
-  // }
+  private async handleOnGet(callback): Promise<void> {
+    this.log.debug('Triggered GET On');
 
-  // private async handleOnSet(value, callback): Promise<void> {
-  //   this.log.debug('Triggered SET On:', value);
-  //   if (value) {
-  //     await this.deviceAPI.turnOn();
-  //   } else {
-  //     await this.deviceAPI.turnOff();
-  //   }
-  //   callback(null);
-  // }
+    // Example: Get the state from the device API
+    const isOn = await this.deviceAPI.getOnState();
+
+    callback(null, isOn);
+  }
+
+  private async handleOnSet(value, callback): Promise<void> {
+    this.log.debug('Triggered SET On:', value);
+
+    // Example: Set the state on the device API
+    if (value) {
+      await this.deviceAPI.turnOn();
+    } else {
+      await this.deviceAPI.turnOff();
+    }
+
+    callback(null);
+  }
 
   getServices(): Service[] {
     return [this.service];
