4675705fb61cdd6d644b13bc49f570a5c18c1bcf
Denis Letnikov <denis.letnikov@gmail.com>
2023-11-21 17:33:14 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ import { AirConditionerAccessory } from './platformAccessory';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerAccessory('AirCondionerAccessory', AirConditionerAccessory);
+  api.registerAccessory('AirConditionerAccessory', AirConditionerAccessory);
 }
