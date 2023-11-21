6021df5132c960c9cec1e2528cf3feffcb2d0db1
Denis Letnikov <denis.letnikov@gmail.com>
2023-11-21 19:00:07 +0200

    fix

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -8,6 +8,13 @@ export class AirConditionerAccessory implements AccessoryPlugin {
   private readonly name: string;
   private readonly deviceAPI: AirConditionerAPI;
   private readonly service: Service;
+  handleTemperatureDisplayUnitsGet: any;
+  handleTemperatureDisplayUnitsSet: any;
+  handleTargetTemperatureGet: any;
+  handleTargetTemperatureSet: any;
+  handleTargetHeatingCoolingStateGet: any;
+  handleTargetHeatingCoolingStateSet: any;
+  handleCurrentHeatingCoolingStateGet: any;
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
@@ -56,7 +63,52 @@ export class AirConditionerAccessory implements AccessoryPlugin {
     callback(null);
   }
 
+  // New methods for controlling the air conditioner
+  private async handleTurnOn(callback): Promise<void> {
+    this.log.debug('Triggered Turn On');
+
+    // Example: Turn on the air conditioner using the new method
+    await this.deviceAPI.turnOn();
+
+    callback(null);
+  }
+
+  private async handleTurnOff(callback): Promise<void> {
+    this.log.debug('Triggered Turn Off');
+
+    // Example: Turn off the air conditioner using the new method
+    await this.deviceAPI.turnOff();
+
+    callback(null);
+  }
+
   getServices(): Service[] {
+    // Adding new characteristics to the service
+    this.service.getCharacteristic(this.api.hap.Characteristic.On)
+      .on('get', this.handleOnGet.bind(this))
+      .on('set', this.handleOnSet.bind(this));
+
+    // Adding new characteristics to the service
+    this.service.addCharacteristic(this.api.hap.Characteristic.OutletInUse);
+    this.service.addCharacteristic(this.api.hap.Characteristic.Name);
+    this.service.addCharacteristic(this.api.hap.Characteristic.CurrentTemperature);
+
+    // Adding new characteristic handlers
+    this.service.getCharacteristic(this.api.hap.Characteristic.TemperatureDisplayUnits)
+      .on('get', this.handleTemperatureDisplayUnitsGet.bind(this))
+      .on('set', this.handleTemperatureDisplayUnitsSet.bind(this));
+
+    this.service.getCharacteristic(this.api.hap.Characteristic.TargetTemperature)
+      .on('get', this.handleTargetTemperatureGet.bind(this))
+      .on('set', this.handleTargetTemperatureSet.bind(this));
+
+    this.service.getCharacteristic(this.api.hap.Characteristic.TargetHeatingCoolingState)
+      .on('get', this.handleTargetHeatingCoolingStateGet.bind(this))
+      .on('set', this.handleTargetHeatingCoolingStateSet.bind(this));
+
+    this.service.getCharacteristic(this.api.hap.Characteristic.CurrentHeatingCoolingState)
+      .on('get', this.handleCurrentHeatingCoolingStateGet.bind(this));
+
     return [this.service];
   }
 }
