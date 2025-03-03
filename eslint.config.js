2bc3652151f224f46055b6e2640590291f8f14a7
Denys Lietnikov <denys.lietnikov@mev.com>
2025-03-03 11:42:38 +0200

    small fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,31 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+/* eslint-env node */
 
-// We pass "recommendedConfig" to let "eslint:recommended" be recognized.
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+// Pass "recommendedConfig" so that "eslint:recommended" is recognized.
 const compat = new FlatCompat({
   baseDirectory: process.cwd(),
   resolvePluginsRelativeTo: process.cwd(),
-  // Provide recommendedConfig so that "eslint:recommended" works
   recommendedConfig: js.configs.recommended,
 });
 
-module.exports = [
-  // Ignore dist folder
+export default [
+  // Ignore the dist folder
   {
     ignores: ['dist'],
   },
 
-  // Use compat.extends for old-style extends.
-  // Remove "plugin:@typescript-eslint/eslint-recommended" 
-  // because it's superseded by "plugin:@typescript-eslint/recommended".
+  // Extend old-style configurations. Note: we pass them as separate arguments.
   ...compat.extends(
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended'
   ),
 
   {
-    // Apply these settings/rules only to TypeScript files
+    // Apply these settings only to TypeScript files.
     files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
@@ -52,10 +51,9 @@ module.exports = [
       'comma-spacing': ['error'],
       'no-multi-spaces': ['warn', { 'ignoreEOLComments': true }],
       'lines-between-class-members': ['warn', 'always', { 'exceptAfterSingleLine': true }],
-
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-  },
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  }
 ];
